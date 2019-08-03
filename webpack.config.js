7c033d0f0159b4dcdd9966d68b4157016a715901
ogrezem <ogrezem@gmail.com>
2019-08-03 18:06:52 +0300

    add vue-style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,19 @@ module.exports = {
                     'vue-style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            indentedSyntax: true
+                        }
+                    }
+                ]
             }
         ]
     },
