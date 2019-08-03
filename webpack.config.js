ae21fe44a2cfde7592baa74a839dd9059c0d903f
ogrezem <ogrezem@gmail.com>
2019-08-03 17:45:13 +0300

    add and configure babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
     output: {
         path: path.resolve(baseDirPath, "dist"),
         filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {test: /\.js$/, use: 'babel-loader'},
+            {test: /\.vue$/, use: 'vue-loader'}
+        ]
     }
 }
